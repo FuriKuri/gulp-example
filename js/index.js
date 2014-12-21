5f943caf16ca01427e2bfacbbfe5d772e1140a88
Theo Pack <tf.pack@gmail.com>
2014-12-21 17:04:45 +0100

    Init

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function sayHello() {
+	console.log('Hello World!');
+}
+
+sayHello();
+
+function add(a, b) {
+	return a + b;
+}
+
+console.log(add(3, 4));
+
+function toUpperCase(string) {
+	return string.toUpperCase();
+}
